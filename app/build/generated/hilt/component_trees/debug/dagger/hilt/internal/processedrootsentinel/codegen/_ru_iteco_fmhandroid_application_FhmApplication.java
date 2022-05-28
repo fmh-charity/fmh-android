15c70bd6da95343dca08f6ffb7b632bc260410f3
Dolgov Dmitry <dolgov2007@inbox.ru>
2022-05-28 15:27:59 +0300

    удалена папка build

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_ru_iteco_fmhandroid_application_FhmApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "ru.iteco.fmhandroid.application.FhmApplication"
-)
-public final class _ru_iteco_fmhandroid_application_FhmApplication {
-}

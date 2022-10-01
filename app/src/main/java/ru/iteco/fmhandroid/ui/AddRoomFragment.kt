64c372106aa5e721948dd4e2d02f52b684ae0da1
Vladi71 <klk71@ya.ru>
2022-10-02 00:33:42 +0300

    Небольшие изменения в Палатах. Устранение выявленных недостатков.

--- app/src/main/java/ru/iteco/fmhandroid/ui/AddRoomFragment.kt
+++ app/src/main/java/ru/iteco/fmhandroid/ui/AddRoomFragment.kt
@@ -1,10 +1,28 @@
 package ru.iteco.fmhandroid.ui
 
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import ru.iteco.fmhandroid.R
+import ru.iteco.fmhandroid.databinding.FragmentAddRoomBinding
+import ru.iteco.fmhandroid.databinding.FragmentCreateEditClaimBinding
 
 class AddRoomFragment : Fragment(R.layout.fragment_add_room) {
 
+    private lateinit var binding: FragmentAddRoomBinding
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentAddRoomBinding.bind(view)
+
+        binding.addPatientImageView.setOnClickListener{
+            if (binding.roomListAddPatientTextView.visibility == View.GONE) {
+                binding.roomListAddPatientTextView.visibility = View.VISIBLE
+            }else
+                binding.roomListAddPatientTextView.visibility = View.GONE
+        }
+
+    }
 }
--- app/src/main/java/ru/iteco/fmhandroid/ui/ListOfBlockFragment.kt
+++ app/src/main/java/ru/iteco/fmhandroid/ui/ListOfBlockFragment.kt
@@ -1,9 +1,6 @@
 package ru.iteco.fmhandroid.ui
 
-import android.os.Bundle
-import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import ru.iteco.fmhandroid.R
 import ru.iteco.fmhandroid.databinding.FragmentListOfBlockBinding
 
@@ -11,12 +8,5 @@ class ListOfBlockFragment: Fragment(R.layout.fragment_list_of_block) {
     private lateinit var binding: FragmentListOfBlockBinding
 
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding = FragmentListOfBlockBinding.bind(view)
 
-        binding.addBlockImageView.setOnClickListener {
-            findNavController().navigate(R.id.action_listOfBlockFragment_to_addBlockFragment)
-        }
-    }
 }

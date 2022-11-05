dddc32d07ef9bb8c13baed87815ec5f2f99b4bb3
VladimirKlekov <99707346+VladimirKlekov@users.noreply.github.com>
2022-11-05 18:00:54 +0300

    изменил dto wish

--- app/src/main/java/ru/iteco/fmhandroid/dto/Wish.kt
+++ app/src/main/java/ru/iteco/fmhandroid/dto/Wish.kt
@@ -6,20 +6,21 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class Wish(
     val id: Int? = null,
-    val patient_id: Int,
+    val patientId: Int,
     val title: String,
     val description: String,
-    val creator_id: Int,
-    val executor_id: Int,
-    val create_date: Long,
-    val plan_execute_date: Long,
-    val fact_execute_date: Long,
+    val creatorId: Int,
+    val executorId: Int,
+    val createDate: Long,
+    val planExecuteDate: Long,
+    val factExecuteDate: Long,
     val status: Status
 ) : Parcelable {
 
     enum class Status {
         OPEN,
         AT_WORK,
-        CLOSED
+        CLOSED,
+        EXECUTED
     }
 }

e460e51d3c9484b73b6f19b4b398da803f3d871c
VladimirKlekov <99707346+VladimirKlekov@users.noreply.github.com>
2022-11-05 21:24:47 +0300

    дополнил PatientApi

--- app/src/main/java/ru/iteco/fmhandroid/api/PatientApi.kt
+++ app/src/main/java/ru/iteco/fmhandroid/api/PatientApi.kt
@@ -3,18 +3,28 @@ package ru.iteco.fmhandroid.api
 import retrofit2.Response
 import retrofit2.http.*
 import ru.iteco.fmhandroid.dto.Patient
+import ru.iteco.fmhandroid.dto.Wish
 
 interface PatientApi {
 
-    @GET("patient")
+    @GET("patients")
     suspend fun getAllPatient(): Response<List<Patient>>
 
-    @PUT("patient")
-    suspend fun editPatientItem(@Body patientItem: Patient): Response<Patient>
-
-    @POST("patient")
+    @POST("patients")
     suspend fun savePatientItem(@Body patientItem: Patient): Response<Patient>
 
-    @DELETE("patient/{id}")
-    suspend fun removePatientItemById(@Path("id") id: Int): Response<Unit>
+    @PUT("patients")
+    suspend fun editPatientItem(@Body patientItem: Patient): Response<Patient>
+
+    @GET("patients/{id}")
+    suspend fun getPatientById(@Path("id") id: Long): Response<Patient>
+
+    @GET("patients/{id}/admission")
+    suspend fun getAllAdmission(@Path("id") id: Long): Response<Unit>
+
+    @GET("patients/{id}/wishes")
+    suspend fun getAllWish(): Response<List<Wish>>
+
+    @GET("patients/{id}/wishes/open-in-progress")
+    suspend fun getWishInOpenAndInProgressStatus(): Response<List<Wish>>
 }

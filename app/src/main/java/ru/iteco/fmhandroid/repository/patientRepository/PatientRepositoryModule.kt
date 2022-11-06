8072e72542b1d05aff2622ac5e6e98fe6b9e2653
VladimirKlekov <99707346+VladimirKlekov@users.noreply.github.com>
2022-11-06 20:49:26 +0300

    создал PatientRepositoryModule

---
+++ app/src/main/java/ru/iteco/fmhandroid/repository/patientRepository/PatientRepositoryModule.kt
@@ -0,0 +1,16 @@
+package ru.iteco.fmhandroid.repository.patientRepository
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+abstract class PatientRepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindPatientRepository(impl: PatientRepositoryImpl): PatientRepository
+}

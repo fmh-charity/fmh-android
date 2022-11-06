12c02c53194ae370d929b84b7f2101659a2bfc51
VladimirKlekov <99707346+VladimirKlekov@users.noreply.github.com>
2022-11-06 20:43:32 +0300

    создал FilterWishListFragment

---
+++ app/src/main/java/ru/iteco/fmhandroid/ui/FilterWishListFragment.kt
@@ -0,0 +1,8 @@
+package ru.iteco.fmhandroid.ui
+
+import androidx.fragment.app.Fragment
+import ru.iteco.fmhandroid.R
+
+class FilterWishListFragment: Fragment(R.layout.fragment_wish_list_filter) {
+
+}
